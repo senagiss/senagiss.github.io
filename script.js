998e4b73c923b405bbc575aea80696ade3557fe0
GISS <164372203+senagiss@users.noreply.github.com>
2024-04-17 13:35:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const words = ["La salud es la verdadera riqueza.", "Tu cuerpo es tu templo.","La verdadera felicidad comienza con una buena salud"]; // Aquí puedes agregar tus palabras
-const colors = ["red", "blue", "green"]; // Colores para cambiar
-
-let index = 0;
-
-function changeWord() {
-  document.getElementById("carousel").innerText = words[index];
-  document.getElementById("carousel").style.color = colors[index];
-  
-  index = (index + 1) % words.length;
-}
-
-setInterval(changeWord, 2000); // Cambia la palabra cada 2 segundos
 
