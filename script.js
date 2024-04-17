fbb2ebac7142972a0005a35fdc8a910dfdaedd9f
GISS <164372203+senagiss@users.noreply.github.com>
2024-04-17 13:32:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const words = ["La salud es la verdadera riqueza.", "Tu cuerpo es tu templo, cuídalo con amor y atención."La verdadera felicidad comienza con una buena salud"]; // Aquí puedes agregar tus palabras
+const words = ["La salud es la verdadera riqueza.", "Tu cuerpo es tu templo.","La verdadera felicidad comienza con una buena salud"]; // Aquí puedes agregar tus palabras
 const colors = ["red", "blue", "green"]; // Colores para cambiar
 
 let index = 0;
