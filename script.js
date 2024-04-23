3221fcfac49622daae1e36094f9fec827a5e7e2b
GISS <164372203+senagiss@users.noreply.github.com>
2024-04-23 09:09:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let goodCount = 0;
+let badCount = 0;
+
+document.getElementById('goodBtn').addEventListener('click', function() {
+    goodCount++;
+    document.getElementById('goodCount').innerText = goodCount;
+});
+
+document.getElementById('badBtn').addEventListener('click', function() {
+    badCount++;
+    document.getElementById('badCount').innerText = badCount;
+});
 
